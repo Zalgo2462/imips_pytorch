5b34065697dc95b3eeac0266ad683e2895eaaa82
logan <logan@mines.sdsmt.edu>
2019-11-04 18:30:13 -0700

    Add slicing to image sequences

--- epipolar_nn/dataloaders/sequence.py
+++ epipolar_nn/dataloaders/sequence.py
@@ -1,32 +1,36 @@
 import glob
-from typing import List, Callable, Sequence
+from abc import ABC
+from typing import List, Callable, Sequence, Iterator
 
 import cv2
 import numpy as np
 
 
-class ImageSequence(Sequence[np.ndarray]):
-    def __getitem__(self, index: int) -> np.ndarray:
-        pass
-
-    def __len__(self) -> int:
-        pass
+class ImageSequence(ABC, Sequence[np.ndarray]):
+    pass
 
 
 class FileListImageSequence(ImageSequence):
     def __init__(self, file_paths: List[str], convert_to_grayscale: bool = False) -> None:
         self._file_paths = file_paths
-        self.convert_to_grayscale = convert_to_grayscale
+        self._convert_to_grayscale = convert_to_grayscale
 
     def filter(self, filter_map: Callable[[str], bool]) -> 'FileListImageSequence':
         new_paths = [x for x in self._file_paths if filter_map(x)]
-        return FileListImageSequence(new_paths, self.convert_to_grayscale)
+        return FileListImageSequence(new_paths, self._convert_to_grayscale)
 
-    def __getitem__(self, index: int) -> np.ndarray:
-        if self.convert_to_grayscale:
-            return cv2.imread(self._file_paths[index], cv2.IMREAD_GRAYSCALE)
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            if self._convert_to_grayscale:
+                return cv2.imread(self._file_paths[index], cv2.IMREAD_GRAYSCALE)
+            else:
+                return cv2.imread(self._file_paths[index], cv2.IMREAD_COLOR)
         else:
-            return cv2.imread(self._file_paths[index], cv2.IMREAD_COLOR)
+            assert isinstance(index, slice)
+            return FileListImageSequence(self._file_paths[index], self._convert_to_grayscale)
+
+    def __reversed__(self) -> Iterator[np.ndarray]:
+        return iter(FileListImageSequence(self._file_paths[::-1], self._convert_to_grayscale))
 
     def __len__(self) -> int:
         return len(self._file_paths)
@@ -35,3 +39,4 @@ class FileListImageSequence(ImageSequence):
 class GlobImageSequence(FileListImageSequence):
     def __init__(self, glob_path: str, recursive=False, convert_to_grayscale=False) -> None:
         super().__init__(sorted(glob.glob(glob_path, recursive=recursive)), convert_to_grayscale)
+
