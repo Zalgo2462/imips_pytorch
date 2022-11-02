69bebb462d11826ff2496983e2c53097782f3a2e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 14:25:07 +0000

    Adding tarfile member sanitization to extractall()

--- imipnet/datasets/blender.py
+++ imipnet/datasets/blender.py
@@ -116,7 +116,26 @@ class BlenderStereoPairs(torch.utils.data.Dataset):
         archive_file = os.path.join(target_dir, file_name)
         tv_data.download_url(url, target_dir, file_name)
         with tarfile.open(archive_file, 'r:gz') as tar:
-            tar.extractall(path=target_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=target_dir)
         os.remove(archive_file)
 
         return os.path.exists(self._project_folder)
